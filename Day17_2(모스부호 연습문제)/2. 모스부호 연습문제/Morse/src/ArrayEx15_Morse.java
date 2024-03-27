93131751f9105f81ab6e5e72c91f55b7c601a936
Park Hong Seok <seok7975@naver.com>
2024-03-27 20:01:32 +0900

    모스부호 연습문제

---
+++ Day17_2(모스부호 연습문제)/2. 모스부호 연습문제/Morse/src/ArrayEx15_Morse.java
@@ -0,0 +1,22 @@
+class ArrayEx15_Morse
+{
+    public static void main(String[] args)
+    {
+        String source = "SOSHELP";
+        String[] morse = {".-", "-...", "-.-.", "-..", "."
+                , "..-.", "--.", "....", "..", ".---"
+                , "-.-", ".-..", "--", "-.", "---"
+                , ".--.", "--.-", ".-.", "...", "-"
+                , "..-", "...-", ".--", "-..-", "-.--"
+                , "--.."};
+
+        String result = "";
+
+        for (int i = 0; i < source.length(); i++)
+        {
+            result += morse[source.charAt(i) - 'A'];
+        }
+        System.out.println("source:" + source);
+        System.out.println("morse:" + result);
+    }
+}
