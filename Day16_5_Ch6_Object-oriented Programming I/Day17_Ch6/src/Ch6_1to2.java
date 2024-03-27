a78e0c8735565d596f2d9bdbfe781267a6dafcee
Park Hong Seok <seok7975@naver.com>
2024-03-27 19:58:05 +0900

    연습문제 Ch6.Object-Oriented Programming

---
+++ Day16_5_Ch6_Object-oriented Programming I/Day17_Ch6/src/Ch6_1to2.java
@@ -0,0 +1,35 @@
+/*[6-1] 다음과 같은 멤버변수를 갖는 클래스를 정의하시오 SutdaCard .
+[6-2] 문제 에서 정의한 SutdaCard클래스에 두 개의 생성자와 info()를 추가해서 실행 6-1
+결과와 같은 결과를 얻도록 하시오.*/
+
+/*
+//보류
+class Ch6_2
+{
+    public static void main(String args[])
+    {
+        SutdaCard card1 = new SutdaCard(3, false);
+        SutdaCard card2 = new SutdaCard();
+        System.out.println(card1.info());
+        System.out.println(card2.info());
+    }
+}
+class SutdaCard
+{
+    private int num ; // 카드의 숫자(1~10 사이의 정수)
+    private boolean isKwang = true;
+
+    SutdaCard(int num, boolean isKwang)
+    {
+        this.num = num;
+        this.isKwang = isKwang;
+    }
+
+    public void info()
+    {
+
+    }
+
+
+}
+*/
