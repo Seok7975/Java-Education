66d0a136efeaff481ee03c35dc59f68053828336
ParkHongSeok <seok7975@naver.com>
2024-03-30 15:01:42 +0900

    배열 실습문제

--- Day18_1(배열 실습문제)/1. 배열 실습문제/Arrangement/src/Arrangement_3.java
+++ Day18_1(배열 실습문제)/1. 배열 실습문제/Arrangement/src/Arrangement_3.java
@@ -7,7 +7,6 @@ class Arrangement_3
     public static void main(String[] args)
     {
 
-
         byte[][] shipBoard = {
                 //  1  2  3  4  5  6  7  8  9
                   { 0, 0, 0, 0, 0, 0, 1, 0, 0 }, // 1
